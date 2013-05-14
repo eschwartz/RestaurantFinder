0f0d0f7c61a9e790b91ecf28aba670659918b6e5
Edan Schwartz <eschwartz@mindboxstudios.com>
2013-05-13 19:27:54 -0500

    Allow Location.geocode to accept geocoder argument - for efficiency

--- web/js/FinderApp/App.Location.js
+++ web/js/FinderApp/App.Location.js
@@ -13,11 +13,12 @@
       return [this.get('restaurant_name'), this.get('city'), this.get('state')].join(", ");
     },
 
-    geocode: function(callback) {
+    geocode: function(callback, geocoder) {
       var self = this;
       callback = _.isFunction(callback)? callback: callback = function() {};
+      geocoder = (geocoder instanceof google.maps.Geocoder)? geocoder: this.geocoder;
 
-      this.geocoder.geocode({ address: this.getFullAddress() }, function(res) {
+      geocoder.geocode({ address: this.getFullAddress() }, function(res) {
         var latLng = res[0].geometry.location;
         self.set('lat', latLng.lat());
         self.set('lng', latLng.lng())
