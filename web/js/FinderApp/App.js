e5a76b067a78cfecfe86e44aabbe93337259f448
Edan Schwartz <eschwartz@mindboxstudios.com>
2013-05-13 21:22:03 -0500

    Bootstrap app with location data
    Geocode loc data on app.start, with loading UI

--- web/js/FinderApp/App.js
+++ web/js/FinderApp/App.js
@@ -1,1 +1,34 @@
 var App = _.isObject(App)? App :  {};
+
+(function() {
+  App.start = function(options) {
+    App.setUIElements();
+    App.bindGlobalEvents();
+
+    $.publish("app:load:start");
+    var locations = new App.LocationCollection(options.locations);
+    locations.geocode(function() {
+      console.log(locations);
+      $.publish("app:load:complete")
+    });
+  }
+
+  App.setUIElements = function() {
+    App.ui = {
+      'appLoading': $('.appLoading')
+    };
+  }
+
+  App.bindGlobalEvents = function() {
+    $.subscribe("app:load:start", App.showLoading);
+    $.subscribe("app:load:complete", App.hideLoading);
+  }
+
+  App.showLoading = function() {
+    App.ui.appLoading.show();
+  }
+
+  App.hideLoading = function() {
+    App.ui.appLoading.hide();
+  }
+})();
