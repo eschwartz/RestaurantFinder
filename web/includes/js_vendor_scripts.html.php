0081366298b7a42a3434a3fcfe74ccfbe787accf
Edan Schwartz <eschwartz@mindboxstudios.com>
2013-05-13 21:13:07 -0500

    Install tinypubsub for PubSubby goodness

--- web/includes/js_vendor_scripts.html.php
+++ web/includes/js_vendor_scripts.html.php
@@ -3,5 +3,5 @@
 <script type="text/javascript" src="js/vendor/underscore.js"></script>
 <script type="text/javascript" src="js/vendor/backbone.js"></script>
 <script type="text/javascript" src="js/vendor/backbone.googlemaps.js"></script>
-<script type="text/javascript" src="js/vendor/underscore.js"></script>
+<script type="text/javascript" src="js/vendor/jquery.ba-tinypubsub.js"></script>
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBiO-EaoD359EqDLeNWweBrJVPoUhX7kFw&sensor=false"></script>
