b75de6fac8fe0ad6865a9613270dea1d83763f02
Edan Schwartz <eschwartz@mindboxstudios.com>
2013-05-16 11:01:05 -0500

    Use full data set in unit test

--- web/js/Tests/FinderApp/App.LocationCollection.spec.js
+++ web/js/Tests/FinderApp/App.LocationCollection.spec.js
@@ -2,26 +2,26 @@ describe("Restaurant Finder App LocationCollection", function() {
   var self = this;
 
   beforeEach(function() {
-    self.locationCollection = new App.LocationCollection([
-      {
-        restaurant_name: "Chipotle",
-        cuisine_type: "Mexican",
-        city: "Minneapolis",
-        state: "Minnesota"
-      },
-      {
-        restaurant_name: "Latuff's Pizzeria",
-        cuisine_type: "Pizza",
-        city: "Minneapolis",
-        state: "Minnesota"
-      },
-      {
-        restaurant_name: "Red's Savoy Pizza",
-        cuisine_type: "Pizza",
-        city: "Minneapolis",
-        state: "Minnesota"
-      }
-    ]);
+    self.locationCollection = new App.LocationCollection(
+      [
+        {"restaurant_name":"Fogo de Chao","cuisine_type":"Brazilian"},
+        {"restaurant_name":"Chipotle","cuisine_type":"Mexican"},
+        {"restaurant_name":"Jimmy John's","cuisine_type":"Sandwiches"},
+        {"restaurant_name":"Broadway Pizza","cuisine_type":"Pizza"},
+        {"restaurant_name":"Smashburger","cuisine_type":"Burgers"},
+        {"restaurant_name":"Leeann Chin","cuisine_type":"Chinese"},
+        {"restaurant_name":"Hoban","cuisine_type":"Korean"},
+        {"restaurant_name":"Red's Savoy Pizza","cuisine_type":"Pizza"},
+        {"restaurant_name":"Holy Land","cuisine_type":"Middle Eastern"},
+        {"restaurant_name":"Manny's Steakhouse","cuisine_type":"Steakhouse"},
+        {"restaurant_name":"Latuff's Pizzeria","cuisine_type":"Pizza"},
+        {"restaurant_name":"Lindey's Steakhouse","cuisine_type":"Steakhouse"},
+        {"restaurant_name":"Dong Yang","cuisine_type":"Korean"},
+        {"restaurant_name":"White Castle","cuisine_type":"Burgers"},
+        {"restaurant_name":"Famous Dave's","cuisine_type":"Barbeque"},
+        {"restaurant_name":"Vincent's","cuisine_type":"French"}
+      ]
+    );
   });
 
   afterEach(function() {
@@ -41,9 +41,9 @@ describe("Restaurant Finder App LocationCollection", function() {
     },15000);
 
     runs(function() {
-      var chipotle = self.locationCollection.at(0);
-      var latuff = self.locationCollection.at(1);
-      var reds = self.locationCollection.at(2);
+      var chipotle = self.locationCollection.at(1);
+      var latuff = self.locationCollection.at(10);
+      var reds = self.locationCollection.at(7);
 
       expect(chipotle.get('lat')).toBeCloseTo(44.748016, 0);
       expect(chipotle.get('lng')).toBeCloseTo(-93.288055, 0);
@@ -66,21 +66,21 @@ describe("Restaurant Finder App LocationCollection", function() {
   it("should search by cuisine", function() {
     self.locationCollection.filterByCuisine("pizza");
 
-    expect(self.locationCollection.length).toEqual(2);
-    expect(self.locationCollection.at(0).get('restaurant_name')).toBe("Latuff's Pizzeria");
+    expect(self.locationCollection.length).toEqual(3);
+    expect(self.locationCollection.at(0).get('restaurant_name')).toBe("Broadway Pizza");
   });
 
   it("should search for matches in cuisine OR restaurant name", function() {
     self.locationCollection.filterByAny("pizza");
 
-    expect(self.locationCollection.length).toEqual(2);
-    expect(self.locationCollection.at(0).get('restaurant_name')).toBe("Latuff's Pizzeria");
+    expect(self.locationCollection.length).toEqual(3);
+    expect(self.locationCollection.at(0).get('restaurant_name')).toBe("Broadway Pizza");
 
     self.locationCollection.revert();
 
     self.locationCollection.filterByAny("ch");
 
-    expect(self.locationCollection.length).toEqual(1);
+    expect(self.locationCollection.length).toEqual(2);
     expect(self.locationCollection.at(0).get('restaurant_name')).toBe("Chipotle");
   });
 
@@ -88,13 +88,13 @@ describe("Restaurant Finder App LocationCollection", function() {
     var collection_orig = _.extend({}, self.locationCollection);
     self.locationCollection.filterByRestaurant("ch");
     self.locationCollection.revert();
-    expect(self.locationCollection.length).toEqual(3);
+    expect(self.locationCollection.length).toEqual(collection_orig.models.length);
     expect(_.isEqual(self.locationCollection.models, collection_orig.models)).toBe(true);
 
     // Run twice, to make sure we're saving the original-original colelction
     self.locationCollection.filterByRestaurant("ch");
     self.locationCollection.revert();
-    expect(self.locationCollection.length).toEqual(3);
+    expect(self.locationCollection.length).toEqual(collection_orig.models.length);
     expect(_.isEqual(self.locationCollection.models, collection_orig.models)).toBe(true);
   });
 });
